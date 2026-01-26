c58b086de5b3deb2d4eeb02895abb910c6fe63ba
Sandeep <sawondeep4@gmail.com>
2026-01-26 17:55:14 +0545

    gateway registry

--- UserServices/src/test/java/com/banking/userservices/UserServicesApplicationTests.java
+++ UserServices/src/test/java/com/banking/userservices/UserServicesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.banking.apigateway;
+package com.banking.userservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class UserServicesApplicationTests {
 
     @Test
     void contextLoads() {
