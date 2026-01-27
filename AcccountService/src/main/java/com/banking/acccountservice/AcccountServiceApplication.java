7aec814468af33a36fc5f45e9ea03e5739c56fd9
Sandeep <sawondeep4@gmail.com>
2026-01-27 15:36:40 +0545

    account service initial

---
+++ AcccountService/src/main/java/com/banking/acccountservice/AcccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking.acccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcccountServiceApplication.class, args);
+    }
+
+}
