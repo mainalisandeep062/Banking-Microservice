6b671af15fca0623d5dc7bd640421d1205e0dd11
Sandeep <sawondeep4@gmail.com>
2026-02-19 10:00:15 +0545

    added exception handling in notificaiton services

---
+++ NotificationService/src/main/java/com/banking/notificationservice/exceptions/ApiResponse.java
@@ -0,0 +1,25 @@
+package com.banking.notificationservice.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> { // Use generics
+    private int status;
+    private String message;
+    private OffsetDateTime timestamp;
+    private T body; // Typed body
+
+    public static <T> ApiResponse<T> success(int status, String message, T body) {
+        return new ApiResponse<>(status, message, OffsetDateTime.now(), body);
+    }
+
+    public static <T> ApiResponse<T> error(int status, String message, T body) {
+        return new ApiResponse<>(status, message, OffsetDateTime.now(), body);
+    }
+}
---
+++ NotificationService/src/main/java/com/banking/notificationservice/exceptions/ApiResponseAdvice.java
@@ -0,0 +1,37 @@
+package com.banking.notificationservice.exceptions;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class ApiResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        // Get the class that is handling the request
+        Class<?> controllerClass = returnType.getContainingClass();
+
+        // Disable wrapping for SpringDoc/Swagger internal controllers
+        return !controllerClass.getPackage().getName().contains("org.springdoc") &&
+                !controllerClass.getPackage().getName().contains("org.springframework.boot.autoconfigure.web");
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+
+        // Double check: don't wrap if it's already an ApiResponse or if the path is for docs
+        String path = request.getURI().getPath();
+        if (body instanceof ApiResponse || path.contains("/v3/api-docs") || path.contains("/swagger-ui")) {
+            return body;
+        }
+
+        return ApiResponse.success(200, "OK", body);
+    }
+}
