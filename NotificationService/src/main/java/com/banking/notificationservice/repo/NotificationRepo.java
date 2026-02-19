4a7eb891c0f68df4f758880fcda6e78b8cb4532f
Sandeep <sawondeep4@gmail.com>
2026-02-19 10:32:33 +0545

    AMQP setup

--- NotificationService/src/main/java/com/banking/notificationservice/repo/NotificationRepo.java
+++ NotificationService/src/main/java/com/banking/notificationservice/repo/NotificationRepo.java
@@ -1,4 +1,10 @@
 package com.banking.notificationservice.repo;
 
-public class NotificationRepo {
+import com.banking.notificationservice.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepo extends JpaRepository<Notification, String> {
+
 }
